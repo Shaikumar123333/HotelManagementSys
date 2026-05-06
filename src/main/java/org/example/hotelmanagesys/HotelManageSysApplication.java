0d6059a295032d2188aa6937b29dd58def79c0cf
shaik umar <umarshaikabdulmohamad@gmail.com>
2026-05-06 17:46:12 +0530

    first

---
+++ src/main/java/org/example/hotelmanagesys/HotelManageSysApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotelmanagesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManageSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManageSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hotelmanagesys/HotelManageSysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotelmanagesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManageSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
